9efdb096a43dac09c277773628dba894f4150a76
Joe-Bernick Rameau <r4meau@gmail.com>
2017-12-19 12:49:17 -0500

    Fix script priority calling

--- MapReport/CustomerReport.php
+++ MapReport/CustomerReport.php
@@ -7,28 +7,17 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <script src="Scripts/jquery-3.2.1.min.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?libraries=geometry,places&ext=.js&key=AIzaSyDDU8G2YqxKA9GByUS0WV406Fw0rwGeOAU"></script>
-    <script src="https://cdn.rawgit.com/googlemaps/v3-utility-library/master/infobox/src/infobox.js?key=AIzaSyDDU8G2YqxKA9GByUS0WV406Fw0rwGeOAU"></script>
-    
-    <script src="Scripts/bootstrap-3.0.3.min.js"></script>
     <link href="Styles/bootstrap-3.0.3.min.css" rel="stylesheet" />
 
     <!-- Include jQuery UI -->
-    <script src="Scripts/jquery-ui-1.12.1.js"></script>
     <link href="Styles/jquery-ui-1.12.1.css" rel="stylesheet" />
 
     <!-- Include Multi Select Dropdown -->
-    <script src="Scripts/bootstrap-multiselect.js"></script>
     <link href="Styles/bootstrap-multiselect.css" rel="stylesheet" />
 
     <!-- Include Date Range Picker -->
-    <script src="Scripts/moment.min.js"></script>
-    <script src="Scripts/daterangepicker-2.1.25.js"></script>
     <link href="Styles/daterangepicker-2.1.25.css" rel="stylesheet" />
 
-    <script src="Scripts/CustomerMapScript.js"></script>
-    <script src="Scripts/CustomerPageScript.js"></script>
     <link href="Styles/MapStyles.css" rel="stylesheet" />
     <link href="Styles/PageStyles.css" rel="stylesheet" />
 
@@ -75,6 +64,17 @@
 
     <div id="map"></div>
 
+    <script src="Scripts/jquery-3.2.1.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?libraries=geometry,places&ext=.js&key=AIzaSyDDU8G2YqxKA9GByUS0WV406Fw0rwGeOAU"></script>
+    <script src="https://cdn.rawgit.com/googlemaps/v3-utility-library/master/infobox/src/infobox.js?key=AIzaSyDDU8G2YqxKA9GByUS0WV406Fw0rwGeOAU"></script>
+    
+    <script src="Scripts/bootstrap-3.0.3.min.js"></script>
+    <script src="Scripts/jquery-ui-1.12.1.js"></script>
+    <script src="Scripts/bootstrap-multiselect.js"></script>
+    <script src="Scripts/moment.min.js"></script>
+    <script src="Scripts/daterangepicker-2.1.25.js"></script>
+    <script src="Scripts/CustomerMapScript.js"></script>
+    <script src="Scripts/CustomerPageScript.js"></script>
     <script>
         google.maps.event.addDomListener(window, "load", initMap);
     </script>
